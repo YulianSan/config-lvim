868fdc3660e995ef8bcd41ae80cb2e393b4cd733
YulianSan <knoob0733@gmail.com>
2023-07-29 00:09:52 -0300

    feat: custom theme and lightspeed

--- config.lua
+++ config.lua
@@ -1,12 +1,5 @@
 vim.opt.relativenumber = true
-
-vim.opt.termguicolors = true
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+lvim.colorscheme = "lunar"
 
 vim.opt.list = true
 
@@ -17,30 +10,24 @@ lvim.plugins = {
         "lukas-reineke/indent-blankline.nvim",
         config = function()
             vim.opt.termguicolors = true
-            vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-            vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-            vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-            vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-            vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-            vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+            vim.cmd [[highlight IndentBlanklineIndent1 guifg=#404040 gui=nocombine]]
+            vim.cmd [[highlight IndentBlanklineIndent2 guifg=#FF8C00 gui=nocombine]]
 
             vim.opt.list = true
-            -- vim.opt.listchars:append("space:⋅")
-            -- vim.opt.listchars:append("eol:↴")
+            vim.opt.listchars:append "space:⋅"
+            vim.opt.listchars:append "eol:↴"
 
             require("indent_blankline").setup {
+                char = "|",
                 space_char_blankline = " ",
                 char_highlight_list = {
-                  "IndentBlanklineIndent1",
-                  "IndentBlanklineIndent2",
-                  "IndentBlanklineIndent3",
-                  "IndentBlanklineIndent4",
-                  "IndentBlanklineIndent5",
-                  "IndentBlanklineIndent6",
+                    "IndentBlanklineIndent1",
+                    "IndentBlanklineIndent2",
                 },
             }
         end
-  },
+    },
+    { 'ggandor/lightspeed.nvim' }
 }
 
 vim.opt.tabstop = 4
